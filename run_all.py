d81c9d571100122254fd358c67eb26a5b2d747fb
cchhnnkk <cchhnnkk@gmail.com>
2014-05-13 22:54:15 +0800

    添加100个testbench

--- run_all.py
+++ run_all.py
@@ -22,20 +22,20 @@ def run_all(filename):
     sat.control(bramfile)
 
 
-def test_uf20_91_1000(n_test):
-    if n_test > 1000:
-        n_test = 1000
+def test_uf20_91_100(n_test):
+    if n_test > 100:
+        n_test = 100
 
-    path = "E:\\sat\\reference\\benchmarks\\satlib-benchmark\\uf20-91\\"
+    path = "testdata\\uf20-91\\"
     if len(sys.argv) == 2:
         start = int(sys.argv[1])
     else:
-        start = 1
+        start = 0
 
     sat.logger.setLevel(logging.CRITICAL)
 
     for i in xrange(start, n_test, 1):
-        filename = "uf20-0%d.cnf" % i
+        filename = "uf20-0%d.cnf" % (i + 1)
         run_all(path + filename)
 
 
@@ -50,4 +50,4 @@ def test_uuf50():
 
 if __name__ == '__main__':
     # test_uf20_91_1000()
-    test_uf20_91_1000(10)
+    test_uf20_91_100(10)
